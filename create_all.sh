fdc2e26d7c9905634da609bb06b396c845f5f0f5
Tristan Jay Mahr <Mahr@Wisc.Edu>
2016-06-24 13:22:10 -0500

    bash script to run several parameters on the script

---
+++ create_all.sh
@@ -0,0 +1,6 @@
+Praat --run --ansi "main.praat" "wavs/input/" "wavs/" 0.020 1.280 >> results.txt
+Praat --run --ansi "main.praat" "wavs/input/" "wavs/" 0.040 1.280 >> results.txt
+Praat --run --ansi "main.praat" "wavs/input/" "wavs/" 0.060 1.280 >> results.txt
+Praat --run --ansi "main.praat" "wavs/input/" "wavs/" 0.080 1.280 >> results.txt
+Praat --run --ansi "main.praat" "wavs/input/" "wavs/" 0.100 1.280 >> results.txt
+
